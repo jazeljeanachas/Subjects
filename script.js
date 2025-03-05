205b5d1a4d5c8593f4e83dcc4c745099aba5865e
Jazz <jazzlingwrites@gmail.com>
2025-03-05 21:26:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch("https://jazeljeanachas.github.io/Subjects/course.json")
         .then(response => response.json())
         .then(data => {
-            console.log("Fetched Data:", data); 
+            console.log("Fetched Data:", data);
             displaySubjects(data);
-            window.allCourses = data;
+            window.allCourses = data; 
         })
         .catch(error => console.error("Error fetching JSON:", error));
 });
 
 const subjectDescriptions = {
+   
     "IT111": "Introduction to Computing",
     "IT112": "Computer Programming 1",
     "PurCom": "Purposive Communication",
@@ -24,7 +25,33 @@ const subjectDescriptions = {
     "ArtApp": "Art Appreciation",
     "UTS": "Understanding the Self",
     "STS": "Science, Technology and Society",
-    "CWTS 2": "Civic Welfare Training Service"
+    "PATHFit": "Exercise-based Fitness Activities",
+    "CWTS 2": "Civic Welfare Training Service",
+    "IT211": "Intro to Human Computer Interaction",
+    "IT212": "Fundamentals of Database Systems",
+    "IT213": "Platform Technologies",
+    "IT214": "Object Oriented Programming",
+    "IT215": "Accounting Principles",
+    "EnviSci": "Environmental Science",
+    "IT221": "Information Management",
+    "IT222": "Networking 1",
+    "IT223": "Web Systems and Technologies",
+    "IT224": "Systems Integration and Architecture 1",
+    "Etch": "Ethics",
+    "Rizal": "Life and Works of Rizal",
+    "PATHFit": "Physical Activity Towards Health and Fitness 4",
+    "IT311": "Information Assurance and Security",
+    "IT312": "Networking 2",
+    "IT313": "Mobile Programming",
+    "IT314": "Software Engineering",
+    "IT315": "IT Elective 1",
+    "ES211a": "Technopreneurship",
+    "IT321": "CAPSTONE Project and Research 1",
+    "IT322": "Integrative Programming and Technologies",
+    "IT323": "Application Development and Emerging Technologies",
+    "IT324": "Quantitative Methods",
+    "IT325": "IT Elective 2",
+    "FreeElec": "Foreign Language"
 };
 
 function displaySubjects(data) {
